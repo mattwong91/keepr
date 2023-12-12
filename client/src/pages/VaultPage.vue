b05caa4367e3e72bb77f52ba216fe1c7f7819042
Matthew Wong <mattwong991@gmail.com>
2023-12-12 12:42:40 -0700

    About to try and get keeps in a vault

--- client/src/pages/VaultPage.vue
+++ client/src/pages/VaultPage.vue
@@ -4,11 +4,44 @@
 
 
 <script>
+import { useRoute } from "vue-router";
 import { AppState } from '../AppState';
-import { computed, reactive, onMounted } from 'vue';
+import { computed, reactive, onMounted, watch } from 'vue';
+import Pop from "../utils/Pop";
+import { vaultsService } from "../services/VaultsService.js";
+import { vaultKeepsService } from "../services/VaultKeepsService.js";
+
 export default {
   setup() {
-    return {}
+    const route = useRoute()
+    const watchableVaultId = route.params.vaultId
+    watch(watchableVaultId, () => {
+      getVaultById();
+      getKeepsInVault();
+    }, { immediate: true });
+
+    async function getVaultById() {
+      try {
+        const vaultId = route.params.vaultId
+        await vaultsService.getVaultById(vaultId)
+      }
+      catch (error) {
+        Pop.error(error)
+      }
+    }
+
+    async function getKeepsInVault() {
+      try {
+        const vaultId = route.params.vaultId
+        await vaultKeepsService.getKeepsInVault(vaultId)
+      }
+      catch (error) {
+        Pop.error(error)
+      }
+    }
+    return {
+      vault: computed(() => AppState.activeVault)
+    }
   }
 };
 </script>
--- client/src/services/VaultKeepsService.js
+++ client/src/services/VaultKeepsService.js
@@ -1,3 +1,14 @@
-class VaultKeepsService { }
+import { VaultKeep } from "../models/VaultKeep"
+import { api } from "./AxiosService"
+
+class VaultKeepsService {
+
+  async getKeepsInVault(vaultId) {
+    const res = await api.get(`api/vaults/${vaultId}/keeps`)
+    const vaultKeeps = res.data.map(obj => new VaultKeep(obj))
+    // TODO explore if I should return/join keep data to the vaultKeep on the backend?
+  }
+
+}
 
 export const vaultKeepsService = new VaultKeepsService()
