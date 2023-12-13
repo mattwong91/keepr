b21a3fccaaaaee0e77704be1c8687528e2587946
Matthew Wong <mattwong991@gmail.com>
2023-12-13 14:24:38 -0700

    Send to profile from navbar login

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -13,9 +13,9 @@
         </div>
         <div class="dropdown-menu dropdown-menu-lg-end dropdown-menu-start p-0" aria-labelledby="authDropdown">
           <div class="list-group">
-            <router-link :to="{ name: 'Account' }">
+            <router-link :to="{ name: 'Profile', params: { profileId: account.id } }">
               <div class="list-group-item dropdown-item list-group-item-action">
-                Manage Account
+                My Profile
               </div>
             </router-link>
             <div class="list-group-item dropdown-item list-group-item-action text-danger selectable" @click="logout">
