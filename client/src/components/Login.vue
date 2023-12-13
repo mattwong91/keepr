c404d3906713fdf549a8b5c8a5826800200bc302
Matthew Wong <mattwong991@gmail.com>
2023-12-13 16:23:13 -0700

    Change login to a dropstart for now

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -5,7 +5,7 @@
       Login
     </button>
     <div v-else>
-      <div class="dropdown my-2 my-lg-0">
+      <div class="dropstart my-2 my-lg-0">
         <div type="button" class="border-0 selectable no-select" data-bs-toggle="dropdown">
           <div v-if="account.picture || user.picture">
             <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded-circle" />
