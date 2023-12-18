38b286adbac2ccf382d7d2fd8887bcc6b1a3dc19
Matthew Wong <mattwong991@gmail.com>
2023-12-18 15:53:10 -0700

    Changed backend DB to AWS

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -1,3 +1,4 @@
+-- Active: 1702937449550@@34.221.176.11@3306@keepr
 CREATE TABLE IF NOT EXISTS accounts(
   id VARCHAR(255) NOT NULL primary key COMMENT 'primary key',
   createdAt DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'Time Created',
